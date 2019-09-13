6e3cfd4063ec43e43929f655ed13973b681a8684
Ethan Cain <ecain121@gmail.com>
2019-09-13 17:05:44 -0500

    First Commit

---
+++ Person UI/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Person UI
+//
+//  Created by Ethan Cain on 9/13/19.
+//  Copyright © 2019 Mizzou. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
